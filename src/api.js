3ad3256f62984ff4c320b0ebc99c87a291fc1e70
GauravSinghNegi16 <gauravnegi88823@gmail.com>
2025-09-08 00:25:11 +0530

    Initial commit

--- src/api.js
+++ src/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-// Use environment variable or fallback to localhost
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  baseURL: "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Attach token automatically if it exists
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -17,7 +15,6 @@ API.interceptors.request.use((config) => {
   return config;
 });
 
-// Optional: handle response errors globally
 API.interceptors.response.use(
   (response) => response,
   (error) => {
